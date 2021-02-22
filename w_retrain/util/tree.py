211128ed236242a6ccfff63a40ea4b32a237e54f
nikooaghaei <nikooaghaei@ualberta.ca>
2021-02-22 23:49:08 +0000

    mango for fixed16 and updated tree

--- w_retrain/util/tree.py
+++ w_retrain/util/tree.py
@@ -2,7 +2,7 @@ import numpy
 
 class Tree(object):
     def __init__(self, data = None, probability = None, loc_tuple = None):
-        self.data = data.cuda()    #image
+        self.data = data    #image
         self.children = []
 #        if probability != None:
 #            probability = probability.amax()    #probablity is of size = (1,10) so is chaged to scalar
@@ -21,9 +21,9 @@ class Tree(object):
         self.children.append(node)  ##if it's the first child to add or it's the highest prob child so far
         return
 
-    def get_depth(self):    #redundanttt maybe!!!!
-        if self.parent:
-            return parent.get_depth() + 1
-        else:
-            return 0    ####single node has depth of 0
+#    def get_depth(self):    #redundanttt maybe!!!!
+ #       if self.parent:
+  #          return parent.get_depth() + 1
+   #     else:
+    #        return 0    ####single node has depth of 0
 
