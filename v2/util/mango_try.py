da0ce3b8140407567cf697965a9dfb0590fb3932
nikooaghaei <nikooaghaei@ualberta.ca>
2021-04-13 20:40:44 +0000

    time efficient mango

--- v2/util/mango_try.py
+++ v2/util/mango_try.py
@@ -3,6 +3,8 @@ import numpy as np
 import copy
 import torch.nn.functional as nnf
 
+import time
+import cProfile
 class Cutout_FIXED(object):
     """Randomly mask out one or more patches from an image.
 
@@ -142,11 +144,14 @@ class MANGO_CUT(object):
         folder_name (str): Name of the folder to save the images on
         n_branches (int): Each node is divided to n_branches nodes -ongoing
     """
-    def __init__(self, model, args):
+    def __init__(self, model, args, data_size = 50000):
         self.model = model
         self.n_branches = args.mng_n_branches
         self.init_length = args.mng_init_len
         self.device = args.device
+        self.data_size = data_size
+        self.n_calls = 0    #used as index of images
+        self.probs = []
 
     def __call__(self, img):
         """
@@ -157,78 +162,99 @@ class MANGO_CUT(object):
             ((mask_len * 2) % h x (mask_len * 2) % h) cut out of it.
             Integer (mask_len * 2) % h : Length of the mask cut out of the image.
         """
-        self.n_calls = self.n_calls + 1
+        # timing=[]
+        # a = time.time()
 
         h = img.size(1)
         w = img.size(2)
-        mask_len = self.init_length
+        # b = time.time()
+        # timing.append(('h/w size', b-a))
         y1 = 0
         y2 = self.init_length
         y3 = h
         x1 = 0
         x2 = self.init_length
         x3 = 0
-        with torch.no_grad(): 
-            root_image = img.view(1,3,32,32).to(self.device)
-            try:
-                pred = self.model(root_image).to(self.device)
-            except:
-                pred = self.model(root_image)
-        value, index = nnf.softmax(pred, dim = 1).max(1)
-        probs.append
-        ###############new
-        # value = nnf.softmax(pred, dim = 1)[0][label]
-        ##################
-        min_prob = value
-        label = index[0]
-        masks = np.ones((self.n_branches, h, w), np.float32)
-        
-        masks[0][y1: y2, x1: x2] = 0. # or any other colors for mask
-        masks[1][y1: y2, x2: x3] = 0.
-        masks[2][y2: y3, x1: x2] = 0.
-        masks[3][y2: y3, x2: x3] = 0.
-        masks = torch.from_numpy(masks)
-        expanding_node = -1 # referring to original img
-        
-        for m, mask in enumerate(masks):
-            mask = mask.expand_as(img)
-            masked_img = img * mask #.to(self.device) # Why error
-            #####building child probability
-            with torch.no_grad():
-                temp_img = masked_img.view(1,3,32,32).to(self.device)
-                try:
-                    pred = self.model(temp_img).to(self.device)
-                except:
-                    pred = self.model(temp_img)
-            softmax_prob = nnf.softmax(pred, dim = 1)
-            prob = softmax_prob[0][label]
+        # timing.append(('before if/else', time.time() - a)) 
+        # print(self.n_calls)
+        if self.n_calls < self.data_size: # still in first epoch
 
-            if prob < min_prob: ######treshold comes here
-                min_prob = prob ###??pointer
-                expanding_node = m
-        if expanding_node == -1:    #no mask- original image
+            # a = time.time()
+
+            with torch.no_grad(): 
+                root_image = img.view(1,3,32,32).to(self.device)
+                try:
+                    pred = self.model(root_image).to(self.device)
+                except:
+                    pred = self.model(root_image)
+            value, index = nnf.softmax(pred, dim = 1).max(1)
+            ###############
+            # value = nnf.softmax(pred, dim = 1)[0][label]
+            ##################
+            min_prob = value
+            label = index[0]
+            masks = np.ones((self.n_branches, h, w), np.float32)
+            
+            masks[0][y1: y2, x1: x2] = 0. # or any other colors for mask
+            masks[1][y1: y2, x2: x3] = 0.
+            masks[2][y2: y3, x1: x2] = 0.
+            masks[3][y2: y3, x2: x3] = 0.
+            masks = torch.from_numpy(masks)
+            branch = -1 # referring to original img
+            
+            for m, mask in enumerate(masks):
+                mask = mask.expand_as(img)
+                masked_img = img * mask #.to(self.device) # Why error
+                #####building child probability
+                with torch.no_grad():
+                    temp_img = masked_img.view(1,3,32,32).to(self.device)
+                    try:
+                        pred = self.model(temp_img).to(self.device)
+                    except:
+                        pred = self.model(temp_img)
+                softmax_prob = nnf.softmax(pred, dim = 1)
+                prob = softmax_prob[0][label]
+
+                if prob < min_prob: ######treshold comes here
+                    min_prob = prob ###??pointer
+                    branch = m
+            
+            self.probs.append(branch)
+            # timing.append(('inside if', time.time() -a ))
+
+        else:
+
+            # a = time.time()
+
+            branch = self.probs[self.n_calls % self.data_size]
+            # timing.append(('inside else', time.time() -a ))
+
+        # a = time.time()
+
+        self.n_calls = self.n_calls + 1
+        if branch == -1:    #no mask- original image
             return img
 
-        elif expanding_node == 0:
-            y = np.random.randint(mask_len)
-            x = np.random.randint(mask_len)
+        elif branch == 0:
+            y = np.random.randint(self.init_length)
+            x = np.random.randint(self.init_length)
 
-        elif expanding_node == 1:
-            y = np.random.randint(mask_len)
-            x = np.random.randint(mask_len, w)
+        elif branch == 1:
+            y = np.random.randint(self.init_length)
+            x = np.random.randint(self.init_length, w)
 
-        elif expanding_node == 2:
-            y = np.random.randint(mask_len, h)
-            x = np.random.randint(mask_len)
+        elif branch == 2:
+            y = np.random.randint(self.init_length, h)
+            x = np.random.randint(self.init_length)
 
-        elif expanding_node == 3:
-            y = np.random.randint(mask_len, h)
-            x = np.random.randint(mask_len, w)
+        elif branch == 3:
+            y = np.random.randint(self.init_length, h)
+            x = np.random.randint(self.init_length, w)
 
-        y1 = np.clip(y - mask_len // 2, 0, h)
-        y2 = np.clip(y + mask_len // 2, 0, h)
-        x1 = np.clip(x - mask_len // 2, 0, w)
-        x2 = np.clip(x + mask_len // 2, 0, w)
+        y1 = np.clip(y - self.init_length // 2, 0, h)
+        y2 = np.clip(y + self.init_length // 2, 0, h)
+        x1 = np.clip(x - self.init_length // 2, 0, w)
+        x2 = np.clip(x + self.init_length // 2, 0, w)
         
         mask = np.ones((h, w), np.float32)
         mask[y1: y2, x1: x2] = 0.
@@ -236,4 +262,10 @@ class MANGO_CUT(object):
         mask = mask.expand_as(img)
         img = img * mask
 
+        # timing.append(('before return', time.time() -a))
+        # sumt = 0
+        # for txt, t in timing:
+        #     sumt += t    
+        # for txt, t in timing:
+        #     print(txt, ':', t*100/sumt)
         return img
