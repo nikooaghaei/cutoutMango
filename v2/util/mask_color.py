27d8c16ecade0d49359d781c7707f247b1f501cb
bedirt <tapkan@ualberta.ca>
2021-10-28 03:57:52 +0000

    main-1phase.py, model_tools.py, mask_color.py and  mango_try.py added which are running for colorful masks

--- v2/util/mask_color.py
+++ v2/util/mask_color.py
@@ -1,47 +1,67 @@
 import torch
 import numpy as np
-import copy
 from torchvision.utils import save_image
-import random
-def maskByColorDistance(img, mask_loc):
+
+def rand_pix(img, mask_loc, mask = None):
+    # get the max value img has
+    # get the min value img has
+    # find the avg
+    # if value is below avg make it equal to min val
+    # if value is above avg make it equal to max val
+    cp_img = img.detach().clone()
+    if mask is None:
+        mask = cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] # c, w, h
+
+        ch_n_255 = []
+        ch_n_0 = []
+        for m, sd in zip([125.3, 123.0, 113.9],[63.0, 62.1, 66.7]):
+            ch_n_255.append((255 - m) / sd)
+            ch_n_0.append((0 - m) / sd)    
+        ####FAR EDGE####
+        # for i, channel in enumerate(cp_img):
+        #     c_max_val = torch.max(channel)
+        #     c_min_val = torch.min(channel)
+        #     c_avg = (c_max_val + c_min_val) / 2
+        #     masked_img[i][masked_img[i] < c_avg] = c_max_val + 1
+        #     masked_img[i][masked_img[i] != c_max_val + 1] = c_min_val
+        #     masked_img[i][masked_img[i] == c_max_val + 1] = c_max_val
+        
+        ####RANDOM#####
+        for channel in range(mask.size(0)):
+            sh = mask[channel].size()
+            mask[channel] = torch.tensor(np.random.uniform(ch_n_0[channel], ch_n_255[channel], size=sh))
+        
+    cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] = mask
+    # save_image(cp_img,'test/'+str(np.random.random())+'.png')
+    return cp_img
+def far_edge(img, mask_loc, mask = None):
     # get the max value img has
     # get the min value img has
     # find the avg
     # if value is below avg make it equal to min val
     # if value is above avg make it equal to max val
     # print(img.shape, mask_loc)
-    cp_img = copy.deepcopy(img)
-    masked_img = cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] # c, w, h
+    cp_img = img.detach().clone()
+    if mask is None:
+        mask = cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] # c, w, h
 
-    ch_n_255 = []
-    ch_n_0 = []
-    for m, sd in zip([125.3, 123.0, 113.9],[63.0, 62.1, 66.7]):
-        ch_n_255.append((255 - m) / sd)
-        ch_n_0.append((0 - m) / sd)    
-    ####FAR EDGE####
-    # for i, channel in enumerate(cp_img):
-    #     c_max_val = torch.max(channel)
-    #     c_min_val = torch.min(channel)
-    #     c_avg = (c_max_val + c_min_val) / 2
-    #     masked_img[i][masked_img[i] < c_avg] = c_max_val + 1
-    #     masked_img[i][masked_img[i] != c_max_val + 1] = c_min_val
-    #     masked_img[i][masked_img[i] == c_max_val + 1] = c_max_val
-    
-    ####RANDOM#####
-    for channel in range(masked_img.size(0)):
-        sh = masked_img[channel].size()
-        masked_img[channel] = torch.tensor(np.random.uniform(ch_n_0[channel], ch_n_255[channel], size=sh))
-    ####another approach ####
-    # max_val = torch.max(cp_img)
-    # min_val = torch.min(cp_img)
-    # avg = (max_val - min_val) / 2
-    # masked_img[masked_img > avg] = masked_img[masked_img > avg] - avg
-    # masked_img[masked_img < avg] = masked_img[masked_img < avg] + avg
-    
-    cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] = masked_img
-    # save_image(cp_img,str(random.random())+'.png')
+        ch_n_255 = []
+        ch_n_0 = []
+        for m, sd in zip([125.3, 123.0, 113.9],[63.0, 62.1, 66.7]):
+            ch_n_255.append((255 - m) / sd)
+            ch_n_0.append((0 - m) / sd)    
+        ####FAR EDGE####
+        # for i, channel in enumerate(cp_img):
+        #     c_max_val = torch.max(channel)
+        #     c_min_val = torch.min(channel)
+        #     c_avg = (c_max_val + c_min_val) / 2
+        #     masked_img[i][masked_img[i] < c_avg] = c_max_val + 1
+        #     masked_img[i][masked_img[i] != c_max_val + 1] = c_min_val
+        #     masked_img[i][masked_img[i] == c_max_val + 1] = c_max_val
+
+    cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] = mask
+    # save_image(cp_img,'test/'+str(np.random.random())+'.png')
     return cp_img
-
 # print(colorByDistance(torch.tensor([-0.5, 0, 0.3 ,0.5])))
 # img = torch.randn(32,32,3)
 
