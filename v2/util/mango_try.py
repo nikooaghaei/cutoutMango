a81a9680bdd3dd1d95ebd4024f5d6345ac5073d7
bedirt <tapkan@ualberta.ca>
2021-10-28 04:18:13 +0000

    same as last commit, change on mango_try,py and mask_color.py for far_edge

--- v2/util/mango_try.py
+++ v2/util/mango_try.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import copy
 import torch.nn.functional as nnf
-from util.mask_color import rand_pix
+from util.mask_color import far_edge, rand_pix
 from torchvision.utils import save_image
 
 class Cutout_FIXED(object):
@@ -1132,12 +1132,111 @@ class Mng_RandColor(object):
         #     save_image(res[i], 'test/final' + str(i)+'.png')
         return res
 
-class Mng_Faredge(object):#TODO
-    def __init__(self, model, args, data_size=50000):
+class Mng_Faredge(object):
+    def __init__(self, model, args):
+        # super().__init__()
         self.model = model
         self.n_branches = args.mng_n_branches
         self.mask_len = args.mng_init_len
         self.device = args.device
+
+    def __call__(self, imgs, true_labels = None):        
+        """
+        Args:
+            img (Tensor): Tensor image of size (C, H, W).
+        Returns:
+            Tensor: Image with one hole of dimension 
+            ((mask_len * 2) % h x (mask_len * 2) % h) cut out of it.
+            Integer (mask_len * 2) % h : Length of the mask cut out of the image.
+        """
+        batch_size = imgs.size(0)
+        c = imgs.size(1)
+        h = imgs.size(2)
+        w = imgs.size(3)
+        temp_mask_len = self.mask_len
+        # change to decide how deep to go. E.g. to go down to 1 pixel mask size set it to 1
+        min_mask_len=7
+        y1 = np.zeros(batch_size, dtype=int)
+        y2 = np.full(batch_size, np.clip(self.mask_len, 0, h), dtype=int)
+        y3 = np.full(batch_size, h, dtype=int)
+        x1 = np.zeros(batch_size, dtype=int)
+        x2 = np.full(batch_size, np.clip(self.mask_len, 0, w), dtype=int)
+        x3 = np.full(batch_size, w, dtype=int)
+        self.model.eval()
+        with torch.no_grad():
+            try:
+                preds = self.model(imgs.to(self.device)).to(self.device)
+            except:
+                preds = self.model(imgs.to(self.device)).to(self.device)
+        # if true_labels: ####TOBE FIXED####
+        #     value = nnf.softmax(preds, dim = 1)[0][true_labels]
+        #     label = true_labels
+        # else:
+        min_probs, labels = preds.max(1)
+        #########################################
+        # min_probs = torch.full((128,), 1000.)
+        min_probs = min_probs.fill_(1000.)
+        #########################################
+        # res = copy.deepcopy(imgs)
+        res = imgs.detach().clone()
+        while temp_mask_len > min_mask_len:
+            masked_imgs = np.ones((batch_size, self.n_branches, c, h, w), np.float32)
+            masked_imgs = torch.from_numpy(masked_imgs)
+
+            for i in range(batch_size):
+                masked_imgs[i] = torch.stack([
+                far_edge(imgs[i], [y1[i], y2[i], x1[i], x2[i]]),
+                far_edge(imgs[i], [y1[i], y2[i], x2[i], x3[i]]),
+                far_edge(imgs[i], [y2[i], y3[i], x1[i], x2[i]]),
+                far_edge(imgs[i], [y2[i], y3[i], x2[i], x3[i]])])
+
+            branch = np.full(batch_size, -1) # referring to original img
+            #####building child probability
+            self.model.eval()
+            with torch.no_grad():
+                temp_imgs = masked_imgs.view(batch_size*self.n_branches, c, h, w).to(self.device)
+                try:
+                    preds = self.model(temp_imgs).to(self.device)
+                except:
+                    preds = self.model(temp_imgs)
+            preds = preds.view(batch_size, self.n_branches, preds.size(1))
+            
+            temp_mask_len = temp_mask_len//2
+            for i in range(batch_size):
+                # softmax_probs = nnf.softmax(preds[i], dim = 1)
+                probs = [preds[i][j][labels[i]] for j in range(self.n_branches)]
+
+                for j in range(self.n_branches):
+                    if probs[j] < min_probs[i]: ######treshold comes here
+                        min_probs[i] = probs[j]
+                        branch[i] = j
+                        res[i] = masked_imgs[i][j]
+                        # save_image(res[i], 'test/' + str(i)+str(j)+'.png')
+                # if branch[i] == -1:
+                #     continue
+                if branch[i] == 0:
+                    y3[i] = y2[i]
+                    y2[i] = np.clip(y1[i] + temp_mask_len, 0, y2[i])
+                    x3[i] = x2[i]
+                    x2[i] = np.clip(x1[i]+temp_mask_len, 0, x2[i])
+                elif branch[i] == 1:
+                    y3[i] = y2[i]
+                    y2[i] = np.clip(y1[i] + temp_mask_len, 0, y2[i])
+                    x1[i] = x2[i]
+                    x2[i] = np.clip(x2[i]+temp_mask_len, 0, x3[i])
+                elif branch[i] == 2:
+                    y1[i] = y2[i]
+                    y2[i] = np.clip(y2[i] + temp_mask_len, 0, y3[i])
+                    x3[i] = x2[i]
+                    x2[i] = np.clip(x1[i]+temp_mask_len, 0, x2[i])
+                elif branch[i] == 3:
+                    y1[i] = y2[i]
+                    y2[i] = np.clip(y2[i] + temp_mask_len, 0, y3[i])
+                    x1[i] = x2[i]
+                    x2[i] = np.clip(x2[i]+temp_mask_len, 0, x3[i])
+        # for i in range(res.size(0)):
+        #     save_image(res[i], 'test/final' + str(i)+'.png')
+        return res
 
 class MngCut_RandomColor(object):   
     """If any of 1/4 of the root is chosen by MANGo, it applies Cutout on that quarter.
--- v2/util/mask_color.py
+++ v2/util/mask_color.py
@@ -3,11 +3,6 @@ import numpy as np
 from torchvision.utils import save_image
 
 def rand_pix(img, mask_loc, mask = None):
-    # get the max value img has
-    # get the min value img has
-    # find the avg
-    # if value is below avg make it equal to min val
-    # if value is above avg make it equal to max val
     cp_img = img.detach().clone()
     if mask is None:
         mask = cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] # c, w, h
@@ -17,15 +12,6 @@ def rand_pix(img, mask_loc, mask = None):
         for m, sd in zip([125.3, 123.0, 113.9],[63.0, 62.1, 66.7]):
             ch_n_255.append((255 - m) / sd)
             ch_n_0.append((0 - m) / sd)    
-        ####FAR EDGE####
-        # for i, channel in enumerate(cp_img):
-        #     c_max_val = torch.max(channel)
-        #     c_min_val = torch.min(channel)
-        #     c_avg = (c_max_val + c_min_val) / 2
-        #     masked_img[i][masked_img[i] < c_avg] = c_max_val + 1
-        #     masked_img[i][masked_img[i] != c_max_val + 1] = c_min_val
-        #     masked_img[i][masked_img[i] == c_max_val + 1] = c_max_val
-        
         ####RANDOM#####
         for channel in range(mask.size(0)):
             sh = mask[channel].size()
@@ -44,20 +30,15 @@ def far_edge(img, mask_loc, mask = None):
     cp_img = img.detach().clone()
     if mask is None:
         mask = cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] # c, w, h
-
-        ch_n_255 = []
-        ch_n_0 = []
-        for m, sd in zip([125.3, 123.0, 113.9],[63.0, 62.1, 66.7]):
-            ch_n_255.append((255 - m) / sd)
-            ch_n_0.append((0 - m) / sd)    
+  
         ####FAR EDGE####
-        # for i, channel in enumerate(cp_img):
-        #     c_max_val = torch.max(channel)
-        #     c_min_val = torch.min(channel)
-        #     c_avg = (c_max_val + c_min_val) / 2
-        #     masked_img[i][masked_img[i] < c_avg] = c_max_val + 1
-        #     masked_img[i][masked_img[i] != c_max_val + 1] = c_min_val
-        #     masked_img[i][masked_img[i] == c_max_val + 1] = c_max_val
+        for channel in cp_img:
+            c_max_val = torch.max(channel)
+            c_min_val = torch.min(channel)
+            c_avg = (c_max_val + c_min_val) / 2
+            mask[mask < c_avg] = c_max_val + 1
+            mask[mask != c_max_val + 1] = c_min_val
+            mask[mask == c_max_val + 1] = c_max_val
 
     cp_img[:, mask_loc[0]:mask_loc[1], mask_loc[2]:mask_loc[3]] = mask
     # save_image(cp_img,'test/'+str(np.random.random())+'.png')
